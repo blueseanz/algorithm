900420bb8829f68fd72560e750241e55264e6c95
Ming Feng <f.ming@cbanewzealand.org.nz>
2020-06-22 13:44:43 +1200

    create solution and add data structure, add Stack to it

---
+++ DataStructure/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    public class Stack
+    {
+        private int p_index;
+        private ArrayList list;
+
+        public Stack() 
+        {
+            list = new ArrayList();
+            p_index = -1;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public void push(object item)
+        {
+            list.Add(item);
+            p_index++;
+        }
+
+        public object pop()
+        {
+            object obj = list[p_index];
+            list.RemoveAt(p_index);
+            p_index--;
+            return obj;
+        }
+
+        public void clear()
+        {
+            list.Clear();
+            p_index = -1;
+        }
+
+        public object peak()
+        {
+            return list[p_index];
+        }
+    }
+}
---
+++ MainAlgorithmn/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DataStructure;
+
+namespace MainAlgorithmn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack alist = new Stack();
+
+            string ch;
+            string word = "eyjjjye";
+            bool isPalindrome = true;
+
+            for(int x=0; x < word.Length; x++)
+            {
+                alist.push(word.Substring(x, 1));
+            }
+
+            int pos = 0;
+            while (alist.Count > 0)
+            {
+                ch = alist.pop().ToString();
+                if(ch != word.Substring(pos, 1))
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                pos++;
+            }
+
+            if(isPalindrome)
+                Console.WriteLine(word + " is a palindrome.");
+            else
+                Console.WriteLine(word + " is not a palindrome.");
+            Console.Read();
+        }
+    }
+}
